6998fb93137d2d52e2d6f2ee9d78c2ccce5459d4
Matthew Mueller <MuellerMatthew@users.noreply.github.com>
2016-04-18 17:26:27 -0400

    Update leaflet.conditionalLayer.js

--- dist/leaflet.conditionalLayer.js
+++ dist/leaflet.conditionalLayer.js
@@ -1,30 +1,65 @@
 // Loads a given number of markers visible in the frame
 // Inspired on the work of Ishmael Smyrnow
 
-  L.ConditionalMarkers = L.FeatureGroup.extend({
+  L.ConditionalMarkers = L.GeoJSON.extend({
   	
   	options: {
-  		maxMarkers: 500
+		/**
+		 * Defines the maximum number of markers or sub-layers which the layer can be desplay on the screen at any
+		 * time. Note, this can also be passed as a function which returns the number. 
+		 * @type {Number}
+		 */
+  		maxMarkers: 500,
+  		/**
+		 * Sorts the markers before selecting the maximum number of results. 
+		 */
   		DisplaySort: function(a, b) {
-				return b - a;
-			}
+			return b - a;
+		},
+		/**
+		 * Filters locations as they are displayed on the map. Locations which fail the filter are not displayed, but 
+		 * are retained in the layer.  This allows for faster on the fly changing of the visibility of locations with checkboxes, etc. 
+		 */
+		displayFilter: function( feature ) {
+			return true;
+		},
+		
+		/**
+		 * If autohide is true, the layer will listen for zoom end, and check if the layer should still be displayed on the map. 
+		 * @type {Boolean}
+		 */
+		autohide: true,
+
+		/**
+		 * The minimum zoom level in which the layer should show up on the map.
+		 * @type {Integer}
+		 */
+		minZoom: 13,
+
+		/**
+		 * The maximum zoom level in which the layer should show up on the map.
+		 * @type {Integer}
+		 */
+		maxZoom: Infinity
   	},
-  	initialize: function (layers, options) {
+  	initialize: function (geojson, options) {
   		L.setOptions(this, options);
   		
 		this._layers = {};
 
 		var i, len;
 
-		if (layers) {
-			for (i = 0, len = layers.length; i < len; i++) {
-				this.addLayer(layers[i]);
+		if (geojson) {
+			len = geojson.length;
+			for (i = 0; i < len; i++) {
+				this.addLayer(geojson[i]);
 			}
 		}
 		
       this._markers= [];
     	this.onMap= false;
 	},
+	
     setMaxMarkers: function (i) {
     	if (this.options.maxMarkers != undefined) {
 	    	this.options.maxMarkers=i;
@@ -63,8 +98,11 @@
       }
 
       // Add layer to the map
-      this._layer = new L.FeatureGroup();
+      this._layer = new L.geoJson(null, this.options);
+      layer._parent = this;
+      layer.options.filter = function(){return true;}//preventing the child from inheriting the parents filter. 
       this._map.addLayer(this._layer);
+      
       this._addMarkers();
       this._cleanupMarkers();
 
@@ -76,44 +114,85 @@
       this.onMap = false;
       map.off("moveend", this._update);
     },
+    
+	/**
+	 * Checks whether a layer should be visible on the map based upon its minZoom, and then
+	 * changes the visibility of the layer on the map.
+	 * @param  {number} z current zoom level of the map
+	 * @return {boolean}   Returns whether the layer is visible on the map.
+	 */
+	checkZoom: function(z) {
+		var map = this._map || this._oldMap,
+			opt = this.options;
+			minZ = opt.minZoom,
+			maxZ = opt.maxZoom;
+	
+		if ( (z  < minZ) || (z > maxZ) ) {
+			if ( map.hasLayer(this) ) {
+				map.removeLayer(this);
+				this._oldMap = map;
+				this.autoHidden = true;
+			}
+		} else {
+			if ( !map.hasLayer(this) && this.autoHidden === true ) {
+				map.addLayer(this);
+				this._oldMap = undefined;
+				this.autoHidden = false;
+			}
+		}
+		return map.hasLayer(this);
+	},
 
     _update: function (e) {
+    	if(e && this.options.autohide !== false) {
+		this.checkZoom(e.target._zoom);
+	}
       // Perform updates to markers on map
       if (this.onMap==true) {
-        this._removeMarkers();
-        this._addMarkers();
-        this._cleanupMarkers();
+        this._removeMarkers()._addMarkers()._cleanupMarkers();
       }
     },
 
     _addMarkers: function () {
       // Add select markers to layer; skips existing ones automatically
       var i, marker,
-      options = this.options;
+      options = this.options,
+      list = [];
 
       var markers = this._getMarkersInViewport(this._map);
+      
+      for (i = markers.length - 1; i >= 0; i--) {
+		if( options.displayFilter(markers[i].feature) ) {
+			list.push(markers[i]);
+		}
+	}
       markers.sort(options.DisplaySort);
 
       for (i = 0; i < markers.length && i < options.maxMarkers; i++) {
         marker = markers[i];
         this._layer.addLayer(marker);
       }
+      return this;
     },
 
     _removeMarkers: function () {
-      this._layer.clearLayers();
+    	if(this._layer) {
+      		this._layer.clearLayers();
+    	}
+    	return this;
     },
 
     _cleanupMarkers: function () {
-      // Remove out-of-bounds markers
-      // Also keep those with popups or in expanded clusters
-      var bounds = this._map.getBounds();
-
-      this._layer.eachLayer(function (marker) {
-        if (!bounds.contains(marker.getLatLng()) && (!marker._popup || !marker._popup._container)) {
-          this._layer.removeLayer(marker);
-        }
-      }, this);
+	// Remove out-of-bounds markers
+	// Also keep those with popups or in expanded clusters
+	var bounds = this._map.getBounds();
+	
+	this._layer.eachLayer(function (marker) {
+	if (!bounds.contains(marker.getLatLng()) && (!marker._popup || !marker._popup._container)) {
+	  this._layer.removeLayer(marker);
+	}
+	}, this);
+     	return this;
     },
 
     _getMarkersInViewport: function (map) {
@@ -128,9 +207,24 @@
           markers.push(marker);
         }
       }
-
       return markers;
-    }
+    },
+    
+	list: function(n) {
+		return this._markers;
+	},
+    
+	eachLayer: function(method, context) {
+	    	for (var i in this._markers) {
+			method.call(context, this._markers[i]);
+		}
+		return this;
+	},
+    
+	clearLayers: function() {
+		this._markers=[];
+		return this;
+	}
   });
 
   L.conditionalMarkers = function (markers, options) {
